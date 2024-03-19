9b2d82b7f6f50e2d5c1d2b8f46d6d010fe1880b2
Breno Rippel <39842293+BrenoRippel@users.noreply.github.com>
2024-03-19 17:05:45 -0300

    fix em EmprestimoRepository

--- src/main/java/com/senai/m1s09/repository/EmprestimoRepository.java
+++ src/main/java/com/senai/m1s09/repository/EmprestimoRepository.java
@@ -4,19 +4,19 @@ import com.senai.m1s09.entity.EmprestimoEntity;
 import com.senai.m1s09.entity.LivroEntity;
 import com.senai.m1s09.entity.MembroEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface EmprestimoRepository extends JpaRepository<EmprestimoEntity, Long> {
 
-        @Query("update EmprestimoEntity emprestimo set " +
-            " emprestimo.livro = :livro," +
+    @Modifying
+    @Query("update EmprestimoEntity emprestimo set " +
             " emprestimo.membro = :membro, " +
             " emprestimo.dataEmprestimo = :dataEmprestimo " +
             " where emprestimo.id = :id")
-    EmprestimoEntity update(@Param("livro") MembroEntity livro,
-                               @Param("membro") LivroEntity membro,
-                               @Param("dataEmprestimo") String dataEmprestimo,
-                               @Param("dataDevolucao") String dataDevolucao,
-                               @Param("id") Long id);
+    EmprestimoEntity update(@Param("membro") MembroEntity membro,
+                            @Param("dataEmprestimo") String dataEmprestimo,
+                            @Param("id") Long id);
+
 }
