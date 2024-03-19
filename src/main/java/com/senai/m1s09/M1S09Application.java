4a27e1543f41ff374f6f3c5b82b34c2f319e66a0
Breno Rippel <39842293+BrenoRippel@users.noreply.github.com>
2024-03-19 12:23:13 -0300

    first commit

---
+++ src/main/java/com/senai/m1s09/M1S09Application.java
@@ -0,0 +1,13 @@
+package com.senai.m1s09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M1S09Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M1S09Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/senai/m1s09/M1S09ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.m1s09;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M1S09ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
